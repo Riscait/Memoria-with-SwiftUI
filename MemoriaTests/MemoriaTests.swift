5acb0b59a954df50305b3b6da56be9b594865803
Ryunosuke Muramatsu <mr@nerco.jp>
2019-06-29 10:38:34 +0900

    Initial Commit

---
+++ MemoriaTests/MemoriaTests.swift
@@ -0,0 +1,34 @@
+//
+//  MemoriaTests.swift
+//  MemoriaTests
+//
+//  Created by 村松龍之介 on 2019/06/29.
+//  Copyright © 2019 Ryunosuke Muramatsu. All rights reserved.
+//
+
+import XCTest
+@testable import Memoria
+
+class MemoriaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
